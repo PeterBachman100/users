9396194015185484b9975c04c2380044517a051d
Peter Bachman <57814978+PeterBachman100@users.noreply.github.com>
2025-07-17 09:04:08 -0700

    Testing: UserForm, UserList, App

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -11,3 +11,45 @@ test('it shows two inputs and a button', () => {
   expect(inputs).toHaveLength(2);
   expect(button).toBeInTheDocument();
 });
+
+test('it calls onUserAdd when the form is submitted', async () => {
+
+  const mock = jest.fn();
+
+  render(<UserForm onUserAdd={mock} />);
+
+  const nameInput = screen.getByRole('textbox', {
+    name: /name/i
+  });
+  const emailInput = screen.getByRole('textbox', {
+    name: /email/i
+  });
+  
+  await user.click(nameInput);
+  await user.keyboard('name');
+  await user.click(emailInput);
+  await user.keyboard('email');
+
+  const button = screen.getByRole('button');
+  await user.click(button);
+
+  expect(mock).toHaveBeenCalled();
+  expect(mock).toHaveBeenCalledWith({ name: 'name', email: 'email'});
+});
+
+test('empties the two inputs when the form is submitted', async () => {
+  render(<UserForm onUserAdd={()=>{}} />);
+
+  const nameInput = screen.getByRole('textbox', {name: /name/i});
+  const emailInput = screen.getByRole('textbox', {name: /email/i});
+  const button = screen.getByRole('button');
+  
+  await user.click(nameInput);
+  await user.keyboard('name');
+  await user.click(emailInput);
+  await user.keyboard('email');
+  await user.click(button);
+
+  expect(nameInput).toHaveValue('');
+  expect(emailInput).toHaveValue('');
+});
